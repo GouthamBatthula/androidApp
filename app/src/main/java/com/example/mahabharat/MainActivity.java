9c3f19ef0b9130aa06d4493e7c3ca006a7695908
GouthamBatthula <saigouthambatthula@gmail.com>
2024-09-13 13:32:41 +0530

    My Project

---
+++ app/src/main/java/com/example/mahabharat/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mahabharat;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void methodA(View view){
+
+        TextView txtHello=findViewById(R.id.txtMessage);
+        ImageView imageview =(ImageView)findViewById(R.id.img1);
+        imageview.setBackgroundResource(R.drawable.krishna);
+    }
+
+}
